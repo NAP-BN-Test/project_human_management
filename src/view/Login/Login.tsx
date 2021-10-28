04740b8d324d2c67ea20d79b6e69e6ce7940ab4b
NAP-BN-Test <caonv174@gmail.com>
2021-10-28 15:11:48 +0700

    dpme

--- src/view/Login/Login.tsx
+++ src/view/Login/Login.tsx
@@ -1,124 +1,138 @@
-import React, { Component, useEffect, useState } from 'react';
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {Component, useEffect, useState} from 'react';
 // import { useSelector, useDispatch, shallowEqual } from 'react-redux';
-import { RootState } from '../../redux/reducers/index.reducer';
-import { Action } from '../../redux/actions/index.action';
+import {RootState} from '../../redux/reducers/index.reducer';
+import {Action} from '../../redux/actions/index.action';
 
 import {
-    StyleSheet,
-    Text,
-    TextInput,
-    Button,
-    TouchableHighlight,
-    Image,
-    View,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  View,
 } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import stylesLogin from './styleLogin';
-import { Alert, User } from '../../redux/states/type';
-import AsyncStorage from '@react-native-community/async-storage'
+import {Alert, User} from '../../redux/states/type';
+import AsyncStorage from '@react-native-community/async-storage';
 import Toast from 'react-native-simple-toast';
 
 const Login = () => {
-
-    const dispatch = useDispatch()
-    const userLogin: User = useSelector((state: RootState) => state.login.user)
-    const alert: Alert = useSelector((state: RootState) => state.alerts)
-    // luôn luôn chạy
-    useEffect(() => {
-        if (alert.type == 'error') {
-            Toast.show(
-                alert.message,
-                3000
-            )
-        }
-    })
-
-    // Chỉ chạy 1 lần
-    useEffect(() => {
-        const autologin = async () => {
-            userLogin.userName = await AsyncStorage.getItem('username') || ''
-            userLogin.passWord = await AsyncStorage.getItem('password') || ''
-            if (userLogin.userName !== null && userLogin.passWord !== null && userLogin.userName !== '' && userLogin.passWord !== '') {
-                dispatch(Action.act_login(userLogin))
-            }
-        }
-        autologin()
-    }, [])
-
-    // nếu value thay đổi thì sẽ gọi lại use effect
-    // useEffect(() => {
-    //     const autologin = async () => {
-    //         console.log(2);
-
-    //         // userLogin.userName = await AsyncStorage.getItem('username') || null
-    //         // userLogin.passWord = await AsyncStorage.getItem('password') || null
-    //         // infoServer.ip = await AsyncStorage.getItem('ip') || null
-    //         // infoServer.dbName = await AsyncStorage.getItem('dbName') || null
-    //         // if (userLogin.userName !== null && userLogin.passWord !== null && infoServer.ip !== null && infoServer.dbName !== null) {
-    //         //     dispatch(Action.act_login(userLogin, infoServer))
-    //         // }
-    //     }
-    //     autologin()
-    // }, [value])
-
-
-    const [value, setValue] = useState(false);
-    // value ban đầu bằng false
-    const click = async () => {
-        if (value)
-            setValue(false)
-        else
-            setValue(true)
-
+  const dispatch = useDispatch();
+  const userLogin: User = useSelector((state: RootState) => state.login.user);
+  const alert: Alert = useSelector((state: RootState) => state.alerts);
+  // luôn luôn chạy
+  useEffect(() => {
+    if (alert.type == 'error') {
+      Toast.show(alert.message, 3000);
     }
-    // set lại giá trị
+  });
 
+  // Chỉ chạy 1 lần
+  useEffect(() => {
+    const autologin = async () => {
+      userLogin.userName = (await AsyncStorage.getItem('username')) || '';
+      userLogin.passWord = (await AsyncStorage.getItem('password')) || '';
+      if (
+        userLogin.userName !== null &&
+        userLogin.passWord !== null &&
+        userLogin.userName !== '' &&
+        userLogin.passWord !== ''
+      ) {
+        dispatch(Action.act_login(userLogin));
+      }
+    };
+    autologin();
+  }, []);
 
-    const login = async () => {
-        await AsyncStorage.setItem('password', userLogin.passWord)
-        await AsyncStorage.setItem('username', userLogin.userName)
-        if (userLogin.userName !== null && userLogin.passWord !== null) {
-            dispatch(Action.act_login(userLogin))
-        }
+  // nếu value thay đổi thì sẽ gọi lại use effect
+  // useEffect(() => {
+  //     const autologin = async () => {
+  //         console.log(2);
+
+  //         // userLogin.userName = await AsyncStorage.getItem('username') || null
+  //         // userLogin.passWord = await AsyncStorage.getItem('password') || null
+  //         // infoServer.ip = await AsyncStorage.getItem('ip') || null
+  //         // infoServer.dbName = await AsyncStorage.getItem('dbName') || null
+  //         // if (userLogin.userName !== null && userLogin.passWord !== null && infoServer.ip !== null && infoServer.dbName !== null) {
+  //         //     dispatch(Action.act_login(userLogin, infoServer))
+  //         // }
+  //     }
+  //     autologin()
+  // }, [value])
+
+  const [value, setValue] = useState(false);
+  // value ban đầu bằng false
+  const click = async () => {
+    if (value) {
+      setValue(false);
+    } else {
+      setValue(true);
     }
-    return (
-        <View style={stylesLogin.container}>
-            <Image
-                style={stylesLogin.imgbox}
-                source={require('../../asset/img/logo.png')} />
-            <View style={stylesLogin.inputContainer}>
-                <Image style={stylesLogin.inputIcon} source={require('../../asset/img/email.png')} />
-                <TextInput style={stylesLogin.inputs}
-                    placeholder="Email"
-                    onChangeText={(email) => userLogin.userName = email}
-                    keyboardType="email-address"
-                    underlineColorAndroid='transparent' />
-            </View>
+  };
+  // set lại giá trị
 
-            <View style={stylesLogin.inputContainer}>
-                <Image style={stylesLogin.inputIcon} source={require('../../asset/img/padlock.png')} />
-                <TextInput style={stylesLogin.inputs}
-                    placeholder="Password"
-                    onChangeText={(pass) => userLogin.passWord = pass}
+  const login = async () => {
+    await AsyncStorage.setItem('password', userLogin.passWord);
+    await AsyncStorage.setItem('username', userLogin.userName);
+    if (userLogin.userName !== null && userLogin.passWord !== null) {
+      dispatch(Action.act_login(userLogin));
+    }
+  };
+  return (
+    <View style={stylesLogin.container}>
+      <Image
+        style={stylesLogin.imgbox}
+        source={require('../../asset/img/logo.png')}
+      />
+      <View style={stylesLogin.inputContainer}>
+        <Image
+          style={stylesLogin.inputIcon}
+          source={require('../../asset/img/email.png')}
+        />
+        <TextInput
+          style={stylesLogin.inputs}
+          placeholder="Email"
+          onChangeText={(email) => (userLogin.userName = email)}
+          keyboardType="email-address"
+          underlineColorAndroid="transparent"
+        />
+      </View>
 
-                    secureTextEntry={true}
-                    underlineColorAndroid='transparent' />
-            </View>
+      <View style={stylesLogin.inputContainer}>
+        <Image
+          style={stylesLogin.inputIcon}
+          source={require('../../asset/img/padlock.png')}
+        />
+        <TextInput
+          style={stylesLogin.inputs}
+          placeholder="Password"
+          onChangeText={(pass) => (userLogin.passWord = pass)}
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
+        />
+      </View>
 
-            <TouchableHighlight style={[stylesLogin.buttonContainer, stylesLogin.loginButton]} onPress={login}>
-                <Text style={stylesLogin.loginText}>Login</Text>
-            </TouchableHighlight>
+      <TouchableHighlight
+        style={[stylesLogin.buttonContainer, stylesLogin.loginButton]}
+        onPress={login}>
+        <Text style={stylesLogin.loginText}>Login</Text>
+      </TouchableHighlight>
 
-            <TouchableHighlight style={stylesLogin.buttonContainer}>
-                <Text>Forgot your password?</Text>
-            </TouchableHighlight>
+      {/* <TouchableHighlight style={stylesLogin.buttonContainer}>
+        <Text>Forgot your password?</Text>
+      </TouchableHighlight>
 
-            <TouchableHighlight style={stylesLogin.buttonContainer}>
-                <Text>Register</Text>
-            </TouchableHighlight>
-        </View>
-    );
-}
+      <TouchableHighlight style={stylesLogin.buttonContainer}>
+        <Text>Register</Text>
+      </TouchableHighlight> */}
+    </View>
+  );
+};
 
 export default Login;
-
--- src/view/Login/styleLogin.tsx
+++ src/view/Login/styleLogin.tsx
@@ -1,55 +1,59 @@
-import { StyleSheet } from "react-native";
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
+import {block} from 'react-native-reanimated';
 
 const stylesLogin = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#DCDCDC',
-    },
-    imgbox: {
-        marginBottom: 50,
-        display: 'flex',
-        height: 100,
-        width: 300,
-    },
-    inputContainer: {
-        borderBottomColor: '#F5FCFF',
-        backgroundColor: '#FFFFFF',
-        borderRadius: 30,
-        borderBottomWidth: 1,
-        width: 250,
-        height: 45,
-        marginBottom: 20,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    inputs: {
-        height: 45,
-        marginLeft: 16,
-        borderBottomColor: '#FFFFFF',
-        flex: 1,
-    },
-    inputIcon: {
-        width: 30,
-        height: 30,
-        marginLeft: 15,
-        justifyContent: 'center'
-    },
-    buttonContainer: {
-        height: 45,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-        width: 250,
-        borderRadius: 30,
-    },
-    loginButton: {
-        backgroundColor: "#00b5ec",
-    },
-    loginText: {
-        color: 'white',
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+    paddingHorizontal: 30,
+  },
+  imgbox: {
+    marginBottom: 50,
+    display: 'flex',
+    height: 100,
+    width: 300,
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: '100%',
+    marginHorizontal: 30,
+    borderRadius: 30,
+  },
+  loginButton: {
+    backgroundColor: '#00b5ec',
+  },
+  loginText: {
+    color: 'white',
+  },
 });
 export default stylesLogin;
